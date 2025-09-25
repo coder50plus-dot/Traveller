192249c6215d60eeb1c717a990bf995fab469dc1
Arek Rucki <arek@Areks-MacBook-Pro.local>
2025-09-25 21:56:15 +0100

    Initial commit: First GIT project Traveller

---
+++ README.md
@@ -0,0 +1,2 @@
+# First Git project called Traveller
+# Traveller
---
+++ journey_specs.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+const Journey = require('./journey.js');
+const Traveller = require('./traveller.js')
+
+
+    /* A Journey has:
+- a start location.*/
